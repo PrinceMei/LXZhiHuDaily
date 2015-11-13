c962548235c15ae3e6381258bb4ea15e1d25bd96
LonelyTown <710537942@qq.com>
2015-11-13 08:19:51 +0800

    Initial Commit

---
+++ LXZhiHuDaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXZhiHuDaily
+//
+//  Created by Lonely Stone on 15/11/13.
+//  Copyright © 2015年 LonelyStone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXZhiHuDaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXZhiHuDaily
+//
+//  Created by Lonely Stone on 15/11/13.
+//  Copyright © 2015年 LonelyStone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
